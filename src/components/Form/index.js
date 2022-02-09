8e8915a7a8daa514e4dd6d831a3a0243ef4d754e
Stanislav Ivanov <ivanov13sv@gmail.com>
2022-02-10 00:29:52 +0300

    fix pass error

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const Form = () => {
@@ -10,15 +10,15 @@ const Form = () => {
 	const [passwordError, setPasswordError] = useState(
 		'password can not be empty'
 	);
-    const [validForm, setValidForm] = useState(false);
+	const [validForm, setValidForm] = useState(false);
 
-    useEffect(() =>{
-        if (emailError || passwordError){
-            setValidForm(false);
-        } else{
-            setValidForm(true);
-        }
-    },[emailError, passwordError])
+	useEffect(() => {
+		if (emailError || passwordError) {
+			setValidForm(false);
+		} else {
+			setValidForm(true);
+		}
+	}, [emailError, passwordError]);
 
 	const emailHandler = (e) => {
 		setEmail(e.target.value);
@@ -33,25 +33,24 @@ const Form = () => {
 		}
 
 		if (!e.target.value) {
-			setPasswordError('email can not be empty');
+			setEmailError('email can not be empty');
 		}
 		return;
 	};
 
 	const passwordHander = (e) => {
 		setPassword(e.target.value);
-        
+
 		if (e.target.value.length < 3) {
 			setPasswordError('Password must be more 3 simbols');
-		} else if (e.target.value.length > 8) {
-			setPasswordError('Password must be less 8 simbols');
+		} else if (e.target.value.length > 16) {
+			setPasswordError('Password must be less 16 simbols');
 		} else {
-            setPasswordError('')
-        }
+			setPasswordError('');
+		}
 
-		if (!e.target.value){
-
-        }
+		if (!e.target.value) {
+		}
 	};
 
 	const blurHandler = (e) => {
@@ -74,7 +73,7 @@ const Form = () => {
 	return (
 		<div className='form'>
 			<form className='form__body'>
-				<h3 className='form__title'>Registration</h3>
+				<h3 className='form__title'>Authorization</h3>
 				{emailDirty && emailError && (
 					<p className='label email__label'>{emailError} </p>
 				)}
@@ -100,12 +99,12 @@ const Form = () => {
 					placeholder='Enter your password...'
 				/>
 				<button
-                    disabled={!validForm}
+					disabled={!validForm}
 					type='button'
 					onClick={sendHandler}
 					className='form__btn'
 				>
-					Sign in
+					Log in
 				</button>
 			</form>
 		</div>
