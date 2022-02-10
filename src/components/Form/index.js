4d41c0684e83ec6509aa1373d99d022e15258298
Stanislav Ivanov <ivanov13sv@gmail.com>
2022-02-10 16:25:17 +0300

    done 2 custom hooks

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import './style.css';
 
+import { useInput } from '../../hooks/useInput';
+
 const Form = () => {
+	const email = useInput('', { isEmpty: true, minLength: 3, isEmail: false });
+	const password = useInput('', { isEmpty: true, minLength: 5 });
+
+	const validForm = email.inputValid && password.inputValid;
+
 	return (
 		<div className='form'>
 			<form className='form__body'>
 				<h3 className='form__title'>Authorization</h3>
-
+				{email.isDirty && email.isEmpty && (
+					<p className='label email__label'>
+						Поле не может быть пустым
+					</p>
+				)}
+				{email.isDirty && email.emailError && (
+					<p className='label email__label'>wrong email</p>
+				)}
 				<input
+					value={email.value}
+					onChange={email.onChange}
+					onBlur={email.onBlur}
 					className='form__email'
 					name='email'
 					placeholder='Enter your email...'
 				/>
-
+				{password.isDirty && password.minLengthError && (
+					<p className='label password__label'>
+						Пароль не может быть меньше 5
+					</p>
+				)}
 				<input
+					value={password.value}
+					onChange={password.onChange}
+					onBlur={password.onBlur}
 					className='form__password'
 					name='password'
 					type='password'
 					placeholder='Enter your password...'
 				/>
-				<button type='button' className='form__btn'>
+				<button
+					disabled={!validForm}
+					type='button'
+					className='form__btn'
+				>
 					Log in
 				</button>
 			</form>
